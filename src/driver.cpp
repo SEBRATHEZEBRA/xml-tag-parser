c4cd1c980442467f6efea5d0a7badab3fc1c8212
Sebastian Oliver <sebtheoliver@gmail.com>
2021-03-22 19:11:07 +0200

    Created the driver.cpp, tagParser.cpp and tagParser.h files, as well as add the menu to the driver file.

---
+++ src/driver.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "tagParser.h"
+
+int main (void) {
+  std::cout << "r: Read and process tag file" << std::endl;
+  std::cout << "p: Print all the tags" << std::endl;
+  std::cout << "d: Dump tags and data to a file called tag.txt" << std::endl;
+  std::cout << "l: List tag data for given tag to cout" << std::endl;
+  std::cout << "q: Quit" << std::endl;
+
+  return 0;
+}
